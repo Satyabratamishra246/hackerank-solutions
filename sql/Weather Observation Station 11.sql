74deba07461fd372bc85a6f2651e3fd6f2574f07
satyabrata <satyabratamishra246@gmail.com>
2024-07-23 23:17:41 +0530

    delete
    delete the moved files only

--- sql/Weather Observation Station 11.sql
+++
@@ -1,4 +0,0 @@
--- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. 
--- Your result cannot contain duplicates.
-
-SELECT DISTINCT CITY FROM STATION WHERE CITY NOT LIKE "[A,E,I,O,U]%" OR CITY NOT LIKE "%[AE,I,O,U]";
--- sql/Weather Observation Station 8.sql
+++
@@ -1,4 +0,0 @@
--- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. 
--- Your result cannot contain duplicates.
-
-SELECT CITY FROM STATION WHERE CITY LIKE "[A,E,I,O,U]%" AND CITY LIKE "%[A,E,I,O,U]";
